ad56e3be2792023c816caf9a9e640025c65a2c0b
fredmadrona <fredriccamasis.madrona@bicol-u.edu.ph>
2024-09-21 23:04:01 +0800

    added the add_assignment_page and its widgets

--- lib/pages/teacher/add_assignment_page.dart
+++ lib/pages/teacher/add_assignment_page.dart
@@ -1,4 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:school_erp/pages/assignment/assignment_animation.dart';
+import 'package:school_erp/pages/teacher/widget/add_assignment_form.dart';
+
+class AnimationState {
+  final bool animate;
+  final bool isBackNavigation;
+
+  AnimationState({
+    this.animate = false,
+    this.isBackNavigation = false,
+  });
+}
 
 class AddAssignmentPage extends StatefulWidget {
   const AddAssignmentPage({super.key});
@@ -7,62 +19,91 @@ class AddAssignmentPage extends StatefulWidget {
   _AddAssignmentPageState createState() => _AddAssignmentPageState();
 }
 
-class _AddAssignmentPageState extends State<AddAssignmentPage> {
-  String dropdownValue = 'Passed'; // Initial value for the dropdown
+class _AddAssignmentPageState extends State<AddAssignmentPage>
+    with SingleTickerProviderStateMixin {
+  late AssignmentAnimationManager animationManager;
+  bool _animate = false;
+  bool _isTransitioning = false;
+
+  @override
+  void initState() {
+    super.initState();
+    animationManager = AssignmentAnimationManager(vsync: this);
+    _startAnimation();
+  }
+
+  void _handleBackPress() {
+    animationManager.reverseAnimation();
+    Future.delayed(const Duration(milliseconds: 800), () {
+      Navigator.pop(context);
+    });
+  }
+
+  void _startAnimation() {
+    Future.delayed(const Duration(milliseconds: 200), () {
+      setState(() {
+        _animate = true;
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Add Assignment'),
-        backgroundColor: const Color(0xFF5278C1), // Custom color for the app bar
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back_ios, color: Colors.white),
+          onPressed: _handleBackPress,
+        ),
+        title: const Text(
+          'Add Assignment',
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: const Color(0xFF5278C1),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        color: const Color(0xFF5278C1), // Set body background color
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            const Text(
-              'Add Assignment Status',
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            const SizedBox(height: 20),
-            DropdownButtonFormField<String>(
-              value: dropdownValue,
-              icon: const Icon(Icons.arrow_downward),
-              decoration: const InputDecoration(
-                labelText: 'Assignment Status',
-                border: OutlineInputBorder(),
-              ),
-              items: <String>['Passed', 'Failed']
-                  .map<DropdownMenuItem<String>>((String value) {
-                return DropdownMenuItem<String>(
-                  value: value,
-                  child: Text(value),
-                );
-              }).toList(),
-              onChanged: (String? newValue) {
-                setState(() {
-                  dropdownValue = newValue!;
-                });
-              },
-            ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                // Add your save logic here
-              },
-              child: const Text('Save Assignment'),
-              style: ElevatedButton.styleFrom(
-                backgroundColor: const Color(0xFF5278C1), // Matching button color
+            const SizedBox(height: 25),
+            Expanded(
+              // This ensures the Container occupies all available space
+              child: Container(
+                width: double.infinity,
+                decoration: const BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(30.0),
+                    topRight: Radius.circular(30.0),
+                  ),
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    const AddAssignmentForm(),
+                    SizedBox(
+                      height: 150,
+                      width: double.infinity, 
+                      child: Image.asset(
+                        'assets/images/AddAssignmentImage.png', // Replace with your image path
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
           ],
         ),
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    animationManager.dispose();
+    super.dispose();
   }
 }
---
+++ lib/pages/teacher/widget/add_assignment_form.dart
@@ -0,0 +1,216 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:school_erp/pages/teacher/widget/add_assignment_dropdown.dart';
+
+class AddAssignmentForm extends StatefulWidget {
+  const AddAssignmentForm({super.key});
+
+  @override
+  State<AddAssignmentForm> createState() => _AddAssignmentFormState();
+}
+
+class _AddAssignmentFormState extends State<AddAssignmentForm> {
+  String? selectedClass;
+  String? selectedSubject;
+  String? selectedType;
+  String? aiGenerated;
+  List<String> classList = [];
+  List<String> subjectList = [];
+  List<String> typeList = [
+    "Online",
+    "In App",
+    "Take Home",
+  ];
+  List<String> aiOptions = ["Yes", "No"];
+  TextEditingController titleController = TextEditingController();
+  TextEditingController urlController = TextEditingController();
+  TextEditingController instructionsController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    _loadClasses();
+    _loadSubjects();
+  }
+
+  Future<void> _loadClasses() async {
+    String jsonString =
+        await rootBundle.loadString('assets/addAssignmentData/classes.json');
+    List<dynamic> jsonResponse = json.decode(jsonString);
+    setState(() {
+      classList = jsonResponse.cast<String>();
+      selectedClass = classList.isNotEmpty ? classList[0] : null;
+    });
+  }
+
+  Future<void> _loadSubjects() async {
+    String jsonString =
+        await rootBundle.loadString('assets/addAssignmentData/subjects.json');
+    List<dynamic> jsonResponse = json.decode(jsonString);
+    setState(() {
+      subjectList = jsonResponse.cast<String>();
+      selectedSubject = subjectList.isNotEmpty ? subjectList[0] : null;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(15.0),
+      child: SizedBox(
+        width: double.infinity,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text(
+              "Select Class",
+              style: TextStyle(color: Colors.grey, fontSize: 16),
+              textAlign: TextAlign.start,
+            ),
+            const SizedBox(height: 5),
+            AddAssignmentDropdown(
+              value: selectedClass,
+              items: classList,
+              hint: "Choose a class",
+              onChanged: (newValue) {
+                setState(() {
+                  selectedClass = newValue;
+                });
+              },
+            ),
+            const SizedBox(height: 25),
+            const Text(
+              "Select Subject",
+              style: TextStyle(color: Colors.grey, fontSize: 16),
+              textAlign: TextAlign.start,
+            ),
+            const SizedBox(height: 5),
+            AddAssignmentDropdown(
+              value: selectedSubject,
+              items: subjectList,
+              hint: "Choose a subject",
+              onChanged: (newValue) {
+                setState(() {
+                  selectedSubject = newValue;
+                });
+              },
+            ),
+            const SizedBox(height: 25),
+            const Text(
+              "Title",
+              style: TextStyle(color: Colors.grey, fontSize: 16),
+              textAlign: TextAlign.start,
+            ),
+            const SizedBox(height: 5),
+            TextField(
+              controller: titleController,
+              style: const TextStyle(fontWeight: FontWeight.bold),
+              decoration: const InputDecoration(
+                border: UnderlineInputBorder(
+                  borderSide: BorderSide(color: Colors.black),
+                ),
+                hintText: "Type here",
+              ),
+            ),
+            const SizedBox(height: 25),
+            const Text(
+              "Select Type",
+              style: TextStyle(color: Colors.grey, fontSize: 16),
+              textAlign: TextAlign.start,
+            ),
+            const SizedBox(height: 5),
+            AddAssignmentDropdown(
+              value: selectedType,
+              items: typeList,
+              hint: "Choose a type",
+              onChanged: (newValue) {
+                setState(() {
+                  selectedType = newValue;
+                  aiGenerated = null;
+                });
+              },
+            ),
+            const SizedBox(height: 25),
+            // Conditionally show text field based on selected type
+            if (selectedType == "Online") ...[
+              const Text(
+                "URL",
+                style: TextStyle(color: Colors.grey, fontSize: 16),
+              ),
+              const SizedBox(height: 5),
+              TextField(
+                controller: urlController,
+                decoration: const InputDecoration(
+                  border: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.black),
+                  ),
+                  hintText: "Enter the URL",
+                ),
+              ),
+            ] else if (selectedType == "In App") ...[
+              const Text(
+                "AI Generated",
+                style: TextStyle(color: Colors.grey, fontSize: 16),
+              ),
+              const SizedBox(height: 5),
+              AddAssignmentDropdown(
+                value: aiGenerated,
+                items: aiOptions,
+                hint: "Choose an option",
+                onChanged: (newValue) {
+                  setState(() {
+                    aiGenerated = newValue;
+                  });
+                },
+              ),
+            ] else if (selectedType == "Take Home") ...[
+              const Text(
+                "Instructions",
+                style: TextStyle(color: Colors.grey, fontSize: 16),
+              ),
+              const SizedBox(height: 5),
+              TextField(
+                controller: instructionsController,
+                decoration: const InputDecoration(
+                  border: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.black),
+                  ),
+                  hintText: "Enter instructions",
+                ),
+              ),
+            ],
+            const SizedBox(height: 25), // Add spacing before the button
+            ElevatedButton(
+              onPressed: () {
+                // Handle button press (e.g., submit the form)
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: const Color(0xFF5278C1), // Background color
+                minimumSize: const Size(double.infinity, 70),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10.0), // Border radius
+                ), // Full width and fixed height
+              ),
+              child:  Text(
+                selectedType == "In App" ? "Create Assignment" : "SEND",
+                style: const TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                    letterSpacing: 2.0), // Bold text
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    titleController.dispose();
+    urlController.dispose(); // Dispose of the URL controller
+    instructionsController.dispose(); // Dispose of the instructions controller
+    super.dispose();
+  }
+}
