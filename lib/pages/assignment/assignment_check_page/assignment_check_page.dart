92f21c91e801546edd762d07a2d09ad9769ce168
Kelv1nG <kelvinmaglipon@gmail.com>
2024-09-25 09:05:40 +0800

    use custom appbar, animations removed

--- lib/pages/assignment/assignment_check_page/assignment_check_page.dart
+++ lib/pages/assignment/assignment_check_page/assignment_check_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:school_erp/pages/assignment/widgets/assignment_animation_manager.dart';
-import 'widgets/assignment_check_appbar.dart';
+import 'package:school_erp/pages/common_widgets/custom_app_bar.dart';
 import 'widgets/close_button_widget.dart';
 import 'widgets/student_list.dart';
 
@@ -11,76 +10,38 @@ class AssignmentCheckPage extends StatefulWidget {
   _AssignmentCheckPageState createState() => _AssignmentCheckPageState();
 }
 
-class _AssignmentCheckPageState extends State<AssignmentCheckPage>
-    with SingleTickerProviderStateMixin {
-  late AssignmentAnimationManager animationManager;
-
-  @override
-  void initState() {
-    super.initState();
-    animationManager = AssignmentAnimationManager(vsync: this);
-    _startAnimation();
-  }
-
-  void _startAnimation() {
-    animationManager.startAnimation();
-  }
-
+class _AssignmentCheckPageState extends State<AssignmentCheckPage> {
   @override
   Widget build(BuildContext context) {
-    // Use MediaQuery to get the screen height and set a relative app bar height
-    final double screenHeight = MediaQuery.of(context).size.height;
-    final double appBarHeight = screenHeight * 0.12; // 12% of screen height
-
     return Scaffold(
+      appBar: CustomAppBar(
+        title: "Check Assignment",
+        onBackPressed: () => Navigator.pop(context),
+      ),
       body: Stack(
         children: [
           Container(
             color: const Color(0xFF5278C1), // Background remains the same
           ),
-          Positioned(
-            top: appBarHeight, // Start below the AppBar using relative height
-            left: 0,
-            right: 0,
-            bottom: 0, // Extend to the bottom
-            child: Container(
-              decoration: const BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(30.0),
-                  topRight: Radius.circular(30.0),
-                ),
-              ),
-              child: Column(
-                children: [
-                  Expanded(
-                    child: StudentList(),
-                  ),
-                  const CloseButtonWidget(),
-                ],
+          Container(
+            decoration: const BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(30.0),
+                topRight: Radius.circular(30.0),
               ),
             ),
-          ),
-          Positioned(
-            top: 0,
-            left: 0,
-            right: 0,
-            child: AssignmentCheckAppBar(
-              animationManager: animationManager,
-              onBackPressed: () {
-                animationManager.reverseAnimation();
-                Navigator.pop(context); // Close the page
-              },
+            child: Column(
+              children: [
+                Expanded(
+                  child: StudentList(),
+                ),
+                const CloseButtonWidget(),
+              ],
             ),
           ),
         ],
       ),
     );
-  }
-
-  @override
-  void dispose() {
-    animationManager.dispose();
-    super.dispose();
   }
 }
