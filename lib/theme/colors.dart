1e77958a9dea5e0bb412cdb865a7ee091e00049d
fredmadrona <fredriccamasis.madrona@bicol-u.edu.ph>
2024-09-26 20:19:16 +0800

    created colors.dart file

---
+++ lib/theme/colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primaryColor = Color(0xFF5278C1); // Blue
+  static const Color successColor = Color(0xFF0BAC00); // Green
+  static const Color dangerColor = Color(0xFFE92020);  // Red
+  static const Color warningColor = Color(0xFFFE8B22); // Warning
+  static const Color purple = Color(0xFFDD50F2);       // Purple
+  
+}
