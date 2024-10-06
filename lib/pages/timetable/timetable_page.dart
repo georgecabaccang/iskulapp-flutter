69ab44e0fe65d822a678a2a3279f7abb440b5502
fredmadrona <fredriccamasis.madrona@bicol-u.edu.ph>
2024-10-06 13:57:55 +0800

    timetable page initial commit

--- lib/pages/timetable/timetable_page.dart
+++ lib/pages/timetable/timetable_page.dart
@@ -21,7 +21,7 @@ class _TimeTablePageState extends State<TimeTablePage> with SingleTickerProvider
   @override
   void initState() {
     super.initState();
-    loadJsonData(); 
+    // loadJsonData(); 
     animationManager = AssignmentAnimationManager(vsync:this);
     _startAnimation();
   }
@@ -81,7 +81,7 @@ class _TimeTablePageState extends State<TimeTablePage> with SingleTickerProvider
                   child: ListView(
                     padding: const EdgeInsets.all(16.0),
                     children: [
-                          ...events.map((event) => eventsCard(event)).toList(),
+                          //put content here
                         ],
                   ),
                 ),
@@ -94,81 +94,8 @@ class _TimeTablePageState extends State<TimeTablePage> with SingleTickerProvider
   }
 
 
-Widget eventsCard(Map<String, dynamic> event) {
-  return Padding(
-    padding: const EdgeInsets.only(left: 10.0, right: 10.0),
-    child: FractionallySizedBox(
-      widthFactor: 1, 
-      child: Card(
-        margin: const EdgeInsets.symmetric(vertical: 10),
-        elevation: 1,
-        color: AppColors.whiteColor,
-        shape: RoundedRectangleBorder(
-          side: const BorderSide(color: Colors.grey, width: 0.5), 
-          borderRadius: BorderRadius.circular(20.0), 
-        ),  
-        child: Padding(
-          padding: const EdgeInsets.all(10.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                event['title'],
-                style: headingStyle().copyWith(color: Colors.black, fontSize: 18),
-              ),    
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.end,
-                children: [
-                  Container(
-                    height: 75,
-                    width: 75,
-                    decoration: BoxDecoration(
-                      color: Colors.grey.shade300, 
-                      borderRadius: BorderRadius.circular(8.0), 
-                    ),
-                  ),
-                  const SizedBox(height: 10, width: 10), 
-                  Expanded( 
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start, 
-                      mainAxisSize: MainAxisSize.min, 
-                      children: [
-                        Row( 
-                          children: [
-                            const Icon(
-                              Icons.access_time, 
-                              color: AppColors.primaryColor, 
-                              size: 16, 
-                            ),
-                            const SizedBox(width: 5), 
-                            Text(
-                              '${event['date']}, ${event['time']}',
-                              style: bodyStyle().copyWith(color: AppColors.primaryColor),
-                            ),
-                          ],
-                        ),
-                        const SizedBox(height: 5),
-                        Text(
-                          event['description'],
-                          softWrap: true, 
-                          maxLines: 3, 
-                          overflow: TextOverflow.ellipsis, 
-                          style: bodyStyle().copyWith(color: Colors.grey),
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-            ],
-          ),
-        ),
-      ),
-    ),
-  );
-}
 
-
+ 
 
 
 
