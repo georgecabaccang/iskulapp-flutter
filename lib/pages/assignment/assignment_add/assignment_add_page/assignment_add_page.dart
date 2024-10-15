e4e06adabfb139cedf8250cac4dfcd80dfbb5541
fredmadrona <fredriccamasis.madrona@bicol-u.edu.ph>
2024-10-15 20:31:08 +0800

    applied default layout in add assignment page

--- lib/pages/assignment/assignment_add/assignment_add_page/assignment_add_page.dart
+++ lib/pages/assignment/assignment_add/assignment_add_page/assignment_add_page.dart
@@ -1,10 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:school_erp/pages/common_widgets/custom_app_bar.dart';
 import 'widgets/add_assignment_form.dart';
-import 'package:school_erp/pages/EnterExitRoute.dart';
-import 'package:school_erp/pages/common_widgets/app_content.dart';
 import 'package:school_erp/pages/assignment/assignment_add/assignment_setup_page/question_setup_page.dart';
-import 'package:school_erp/theme/colors.dart';
+import 'package:school_erp/pages/common_widgets/default_layout.dart';
 
 class AssignmentAddPage extends StatefulWidget {
   const AssignmentAddPage({super.key});
@@ -15,23 +12,9 @@ class AssignmentAddPage extends StatefulWidget {
 
 class _AssignmentAddPageState extends State<AssignmentAddPage> {
 
-
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: AppColors.primaryColor,
-      body: Column(
-        children: [
-          const CustomAppBar(
-            title: 'Assignment Add ',
-          ),
-          AppContent(
-            content: [
-              AddAssignmentForm()
-            ],
-          ),
-        ],
-      ),
-    );
+    return const DefaultLayout(
+        title: "Add Assignment", content: [AddAssignmentForm()]);
   }
 }
