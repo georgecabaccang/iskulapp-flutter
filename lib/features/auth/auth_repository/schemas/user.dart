8f476a5dd43773bb3999bd088d2fd1a341baf3f5
Kelv1nG <kelvinmaglipon@gmail.com>
2024-10-18 22:34:25 +0800

    update user schema

--- lib/features/auth/auth_repository/schemas/user.dart
+++ lib/features/auth/auth_repository/schemas/user.dart
@@ -6,6 +6,7 @@ part 'user.g.dart';
 @freezed
 class AuthenticatedUser with _$AuthenticatedUser {
   const factory AuthenticatedUser({
+    required int id,
     required String email,
     @JsonKey(name: 'first_name') required String firstName,
     @JsonKey(name: 'last_name') required String lastName,
