e4b814ab94e168f2648e75c56fe6d88f854822ca
Kelv1nG <kelvinmaglipon@gmail.com>
2024-10-18 22:38:08 +0800

    update assignment list page

--- lib/pages/assignment/assignment_list_page/assignment_list_page.dart
+++ lib/pages/assignment/assignment_list_page/assignment_list_page.dart
@@ -1,8 +1,12 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
-import 'package:school_erp/pages/assignment/assignment_list_page/widgets/assignment_add_button.dart';
-import 'widgets/assignment_card.dart';
-import 'package:school_erp/pages/assignment/widgets/assignment_animation_manager.dart';
+import 'package:school_erp/enums/assessment_type.dart';
+import 'package:school_erp/models/assessment.dart';
+import 'package:school_erp/pages/assignment/assignment_add/assignment_add_page/assignment_add_page.dart';
+import 'package:school_erp/pages/common_widgets/app_bar_widgets/add_button.dart';
+import 'package:school_erp/pages/common_widgets/app_content.dart';
 import 'package:school_erp/pages/common_widgets/custom_app_bar.dart';
+import 'widgets/assignment_card.dart';
 import 'package:school_erp/theme/colors.dart';
 
 class AssignmentListPage extends StatefulWidget {
@@ -12,104 +16,56 @@ class AssignmentListPage extends StatefulWidget {
   _AssignmentListPageState createState() => _AssignmentListPageState();
 }
 
-class _AssignmentListPageState extends State<AssignmentListPage>
-    with SingleTickerProviderStateMixin {
-  late AssignmentAnimationManager animationManager;
+class _AssignmentListPageState extends State<AssignmentListPage> {
+  List<Assessment?> _data = [];
+  late StreamSubscription? _subscription;
 
   @override
   void initState() {
     super.initState();
-    animationManager = AssignmentAnimationManager(vsync: this);
-    _startAnimation();
-  }
+    final stream = Assessment.watchLists(AssessmentType.assignment);
 
-  void _startAnimation() {
-    animationManager.startAnimation();
-  }
-
-  void _handleBackPress() {
-    animationManager.reverseAnimation();
-    Future.delayed(animationManager.duration, () {
-      if (mounted) {
-        Navigator.pop(context);
+    _subscription = stream.listen((data) {
+      if (!context.mounted) {
+        return;
       }
+      setState(() {
+        _data = data;
+      });
     });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _subscription?.cancel();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
+      backgroundColor: AppColors.primaryColor,
+      body: Column(
         children: [
-          Container(color: AppColors.primaryColor),
-          AnimatedBuilder(
-            animation: animationManager.controller,
-            builder: (context, child) {
-              return Positioned(
-                top: animationManager.topPosition,
-                left: 0,
-                right: 0,
-                bottom: 0,
-                child: Container(
-                  decoration: const BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(30.0),
-                      topRight: Radius.circular(30.0),
-                    ),
-                  ),
-                  child: FadeTransition(
-                    opacity: animationManager.fadeAnimation,
-                    child: child ?? Container(),
-                  ),
+          CustomAppBar(
+            title: 'Assignment List',
+            trailingWidget: AppBarAddButton(
+                exitPage: widget, enterPage: const AssignmentAddPage()),
+          ),
+          AppContent(
+            content: [
+              Expanded(
+                child: ListView(
+                  padding: const EdgeInsets.all(16.0),
+                  children: _data.map((assessment) {
+                    return AssignmentCard(assessment!);
+                  }).toList(),
                 ),
-              );
-            },
-            child: Column(
-              children: [
-                const SizedBox(height: 25),
-                Expanded(
-                  child: ListView(
-                    padding: const EdgeInsets.all(16.0),
-                    children: const [
-                      AssignmentCard(
-                        subject: 'Mathematics',
-                        title: 'Algebra 1',
-                        assignDate: 'Dec 19, 2024 9:00pm',
-                        lastSubmissionDate: 'Dec 19, 2024 11:59pm',
-                        status: 'TO BE COMPLETED',
-                        statusColor: AppColors.primaryColor,
-                      ),
-                      AssignmentCard(
-                        subject: 'Science',
-                        title: 'Structure of Atoms',
-                        assignDate: '10 Oct 20',
-                        lastSubmissionDate: '30 Oct 20',
-                        status: 'TO BE SUBMITTED',
-                        statusColor: AppColors.primaryColor,
-                      ),
-                      AssignmentCard(
-                        subject: 'English',
-                        title: 'My Bestfriend Essay',
-                        assignDate: '10 Sep 20',
-                        lastSubmissionDate: '30 Sep 20',
-                        status: 'TO BE SUBMITTED',
-                        statusColor:AppColors.primaryColor,
-                      ),
-                    ],
-                  ),
-                ),
-              ],
-            ),
+              ),
+            ],
           ),
         ],
       ),
     );
-  }
-
-  @override
-  void dispose() {
-    animationManager.dispose();
-    super.dispose();
   }
 }
