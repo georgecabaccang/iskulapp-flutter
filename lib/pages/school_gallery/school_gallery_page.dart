6f213e1383ec25f9590db15be85bd0d8806c5a10
Mark <renzomndz09@gmail.com>
2024-10-20 11:46:26 +0800

    Changed to stateless widget

--- lib/pages/school_gallery/school_gallery_page.dart
+++ lib/pages/school_gallery/school_gallery_page.dart
@@ -2,14 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:school_erp/pages/school_gallery/widgets/school_gallery_card.dart'; // Update this to the appropriate path for the gallery widget
 import 'package:school_erp/pages/common_widgets/default_layout.dart';
 
-class SchoolGalleryPage extends StatefulWidget {
+class SchoolGalleryPage extends StatelessWidget {
   const SchoolGalleryPage({super.key});
 
-  @override
-  _SchoolGalleryPageState createState() => _SchoolGalleryPageState();
-}
-
-class _SchoolGalleryPageState extends State<SchoolGalleryPage> {
   @override
   Widget build(BuildContext context) {
     return const DefaultLayout(
