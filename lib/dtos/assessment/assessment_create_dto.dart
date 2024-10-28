04e1acc68679f9726e89c0a6e1a209e8ae6dc2db
Kelv1nG <kelvinmaglipon@gmail.com>
2024-10-29 05:35:22 +0800

    initial create dto for assessment

---
+++ lib/dtos/assessment/assessment_create_dto.dart
@@ -0,0 +1,59 @@
+import 'package:school_erp/enums/assessment_status.dart';
+import 'package:school_erp/enums/assessment_type.dart';
+
+class AssessmentCreateDTO {
+  final AssessmentType assessmentType;
+  final String preparedById;
+  final String subjectYearId;
+  final String sectionId;
+  final String title;
+  final int totalQuestions;
+  final bool randomizeSequence;
+  final DateTime startTime;
+  final DateTime deadLine;
+  final int? durationMinutes;
+  final AssessmentStatus status;
+
+  AssessmentCreateDTO({
+    required this.assessmentType,
+    required this.preparedById,
+    required this.subjectYearId,
+    required this.sectionId,
+    required this.title,
+    required this.totalQuestions,
+    required this.randomizeSequence,
+    required this.startTime,
+    required this.deadLine,
+    this.durationMinutes,
+  }) : status = AssessmentStatus.toBeCompleted;
+}
+
+class AssessmentCreateDTOBuilder {
+  late AssessmentType assessmentType;
+  late String preparedById;
+  late String subjectYearId;
+  late String sectionId;
+  late String title;
+  late int totalQuestions;
+  late bool randomizeSequence;
+  late DateTime startTime;
+  late DateTime deadLine;
+  late int? durationMinutes;
+
+  AssessmentCreateDTOBuilder({this.durationMinutes});
+
+  AssessmentCreateDTO build() {
+    return AssessmentCreateDTO(
+      assessmentType: assessmentType,
+      preparedById: preparedById,
+      subjectYearId: subjectYearId,
+      sectionId: sectionId,
+      title: title,
+      totalQuestions: totalQuestions,
+      randomizeSequence: randomizeSequence,
+      startTime: startTime,
+      deadLine: deadLine,
+      durationMinutes: durationMinutes,
+    );
+  }
+}
