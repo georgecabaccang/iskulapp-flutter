e37aeca4cb0be4fc86f376a6009baa5eb2db7c8f
Kelv1nG <kelvinmaglipon@gmail.com>
2024-10-29 05:37:19 +0800

    create initial table schemas

--- lib/models/tables/assessment_takers_table.dart
+++ lib/models/tables/assessment_takers_table.dart
@@ -1,6 +1,9 @@
 import 'package:powersync/powersync.dart';
 
 const assessmentTakersTable = Table('assessment_takers', [
-  Column.integer('assessment_id'),
-  Column.integer('subject_year_id'),
+  Column.text('assessment_id'),
+  Column.text('subject_year_id'),
+  Column.text('section_id'),
+  Column.text('created_at'),
+  Column.text('updated_at'),
 ]);
---
+++ lib/models/tables/teacher_sections_table.dart
@@ -0,0 +1,6 @@
+import 'package:powersync/powersync.dart';
+
+const teacherSectionsTable = Table('teacher_sections', [
+  Column.text('teacher_id'),
+  Column.text('section_id'),
+]);
