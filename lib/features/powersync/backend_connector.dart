2d9fd4f944303bcd760d9ce656e93d43b8279b4c
Kelv1nG <kelvinmaglipon@gmail.com>
2024-10-30 18:38:52 +0800

    Update backend_connector.dart

--- lib/features/powersync/backend_connector.dart
+++ lib/features/powersync/backend_connector.dart
@@ -32,7 +32,7 @@ class SchoolERPBackendConnector extends PowerSyncBackendConnector {
     if (user == null) {
       throw Exception('User is not logged in');
     }
-    final session = await apiClient.getPowersyncToken(user.id);
+    final session = await apiClient.getPowersyncToken();
     return PowerSyncCredentials(
         endpoint: powersyncUrl, token: session['token']);
   }
