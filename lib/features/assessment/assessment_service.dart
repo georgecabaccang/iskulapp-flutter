5f95ff3a8a4e18d6491f48f67850b5f29579a650
Kelv1nG <kelvinmaglipon@gmail.com>
2024-11-07 16:01:22 +0800

    assessment service
    - bulk creation of assessment taker in  create
    - implemented update
    - added result types

--- lib/features/assessment/assessment_service.dart
+++ lib/features/assessment/assessment_service.dart
@@ -1,9 +1,10 @@
 import 'package:powersync/powersync.dart';
 import 'package:powersync/sqlite3_common.dart';
-import 'package:school_erp/dtos/assessment/assessment_create_dto.dart';
-import 'package:school_erp/dtos/assessment_taker/assessment_taker_create_dto.dart';
-import 'package:school_erp/repositories/assessment_repository.dart';
+import 'package:school_erp/dtos/assessment_dto.dart';
+import 'package:school_erp/dtos/assessment_taker_dto.dart';
+import 'package:school_erp/features/assessment/schemas/assessment_result.dart';
 import 'package:school_erp/features/powersync/db.dart' as ps;
+import 'package:school_erp/repositories/assessment_repository.dart';
 import 'package:school_erp/repositories/assessment_taker_repository.dart';
 
 class AssessmentService {
@@ -18,25 +19,99 @@ class AssessmentService {
         assessmentTakerRepository =
             AssessmentTakerRepository(database: database ?? ps.db);
 
-  Future<(bool, (ResultSet, ResultSet))> create({
-    required AssessmentCreateDTO assessmentCreateDTO,
-    required AssessmentTakerCreateDTO assessmentTakerCreateDTO,
+  Future<AssessmentWithTakersCreateResult> create({
+    required AssessmentCreateDTO assessmentDTO,
+    required List<AssessmentTakerCreateDTO> assessmentTakerDTOs,
   }) async {
     try {
       final result = await db.writeTransaction((tx) async {
-        final ResultSet resultAssessment = await assessmentRepository
-            .createTransaction(assessmentCreateDTO, tx);
+        final ResultSet assessment =
+            await assessmentRepository.create(assessmentDTO, tx);
 
-        assessmentTakerCreateDTO.assessmentId = resultAssessment.first['id'];
+        for (final assessmentTakerDTO in assessmentTakerDTOs) {
+          assessmentTakerDTO.assessmentId = assessment.first['id'];
+        }
 
-        final ResultSet resultAssessmentTaker = await assessmentTakerRepository
-            .createTransaction(assessmentTakerCreateDTO, tx);
+        final List<ResultSet> assessmentTakers =
+            await assessmentTakerRepository.bulkCreate(assessmentTakerDTOs, tx);
 
-        return (true, (resultAssessment, resultAssessmentTaker));
+        return AssessmentWithTakersCreateResult(
+          assessment: assessment,
+          assessmentTakers: assessmentTakers,
+        );
       });
       return result;
     } catch (e) {
       rethrow;
+    }
+  }
+
+  Future<AssessmentWithTakersUpdateResult> update({
+    AssessmentUpdateDTO? assessmentDTO,
+    List<AssessmentTakerUpdateDTO>? assessmentTakerDTOs,
+    List<String>? removeAssessmentTakerIds,
+  }) async {
+    _validateUpdateArgs(assessmentTakerDTOs, removeAssessmentTakerIds);
+
+    ResultSet? assessment;
+    var assessmentTakers = <ResultSet>[];
+    ResultSet? removedAssessmentTakers;
+
+    try {
+      final result = await db.writeTransaction((tx) async {
+        if (assessmentDTO != null) {
+          assessment = await assessmentRepository.update(assessmentDTO, tx);
+        }
+
+        if (assessmentTakerDTOs != null) {
+          final assessmentTakerFutures = <Future<ResultSet>>[];
+          for (final assessmentTakerDTO in assessmentTakerDTOs) {
+            assessmentTakerFutures.add(
+              assessmentTakerRepository.createOrUpdate(
+                assessmentTakerDTO,
+                ['id'],
+                tx,
+              ),
+            );
+          }
+          assessmentTakers = await Future.wait(assessmentTakerFutures);
+        }
+
+        if (removeAssessmentTakerIds != null) {
+          removedAssessmentTakers = await assessmentTakerRepository.delete(
+              removeAssessmentTakerIds, tx);
+        }
+
+        return AssessmentWithTakersUpdateResult(
+          assessment: assessment,
+          assessmentTakers: assessmentTakers,
+          removedAssessmentTakers: removedAssessmentTakers,
+        );
+      });
+      return result;
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  /// validate that item that is being updated is not being removed at the same time
+  void _validateUpdateArgs(
+    List<AssessmentTakerUpdateDTO>? assessmentTakerDTOs,
+    List<String>? removeAssessmentTakerIds,
+  ) {
+    if (assessmentTakerDTOs != null && removeAssessmentTakerIds != null) {
+      final updateIds = assessmentTakerDTOs
+          .where((dto) => dto.id != null)
+          .map((dto) => dto.id!)
+          .toSet();
+      final removeIds = removeAssessmentTakerIds.toSet();
+      final intersection = updateIds.intersection(removeIds);
+
+      if (intersection.isNotEmpty) {
+        throw ArgumentError(
+          'Cannot update and remove the same assessment takers: ${intersection.join(", ")}',
+        );
+      }
     }
   }
 }
