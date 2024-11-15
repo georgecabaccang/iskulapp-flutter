80e439d99e9e7270c60216a9a06230801bd5a185
Kelv1nG <kelvinmaglipon@gmail.com>
2024-11-15 19:50:02 +0800

    update enums

--- lib/enums/assessment_type.dart
+++ lib/enums/assessment_type.dart
@@ -1,15 +1,25 @@
 enum AssessmentType {
-  assignment('assignment'),
-  quiz('quiz'),
-  exam('exam');
+  assignment('assignment', 'Assignment'),
+  quiz('quiz', 'Quiz'),
+  exam('exam', 'Exam');
 
   final String value;
+  final String displayName;
 
-  const AssessmentType(this.value);
+  const AssessmentType(this.value, this.displayName);
 
   static AssessmentType fromString(String value) {
     return values.firstWhere(
       (v) => v.value == value,
+      orElse: () => throw ArgumentError('Invalid AssessmentType value: $value'),
+    );
+  }
+
+  static AssessmentType fromDisplayName(String displayName) {
+    return values.firstWhere(
+      (v) => v.displayName.toLowerCase() == displayName.toLowerCase(),
+      orElse: () => throw ArgumentError(
+          'Invalid AssessmentType display name: $displayName'),
     );
   }
 }
