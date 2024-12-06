6022d2d214926d55f07796a0fd120bcfd60ffa51
georgesycabaccang <georgesycabaccang@gmail.com>
2024-12-06 16:41:48 +0800

    Change padding values of custom_item_card.dart to make it more responsive.

--- lib/pages/common_widgets/cards/custom_item_card.dart
+++ lib/pages/common_widgets/cards/custom_item_card.dart
@@ -3,43 +3,51 @@ import 'package:school_erp/features/transition/clean_slide_transition.dart';
 import 'package:school_erp/theme/colors.dart';
 
 class CustomItemCard extends StatelessWidget {
-  final Widget slideRoute;
-  final List<Widget> itemContents;
+    final Widget slideRoute;
+    final List<Widget> itemContents;
 
-  const CustomItemCard(
-      {super.key, required this.slideRoute, required this.itemContents});
+    const CustomItemCard(
+    {super.key, required this.slideRoute, required this.itemContents});
 
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 10.0),
-        child: FractionallySizedBox(
-            widthFactor: 1,
-            child: InkWell(
-              onTap: () {
-                Navigator.push(
-                  context,
-                  createSlideRoute(
-                    slideRoute,
-                  ),
-                );
-              },
-              child: Card(
-                margin: const EdgeInsets.only(bottom: 20.0),
-                elevation: 1,
-                color: AppColors.whiteColor,
-                shape: RoundedRectangleBorder(
-                  side: const BorderSide(color: Colors.grey, width: 0.5),
-                  borderRadius: BorderRadius.circular(20.0),
+    @override
+    Widget build(BuildContext context) {
+        double screenWidth = MediaQuery.of(context).size.width;
+        double screenHeight = MediaQuery.of(context).size.height;
+
+        double horizontalPadding = screenWidth * 0.05; 
+        double verticalPadding = screenHeight * 0.02; 
+
+        return Padding(
+            padding: EdgeInsets.symmetric(horizontal: horizontalPadding),
+            child: FractionallySizedBox(
+                widthFactor: 1,
+                child: InkWell(
+                    onTap: () {
+                        Navigator.push(
+                            context,
+                            createSlideRoute(
+                                slideRoute,
+                            ),
+                        );
+                    },
+                    child: Card(
+                        margin: EdgeInsets.only(bottom: verticalPadding),
+                        elevation: 1,
+                        color: AppColors.whiteColor,
+                        shape: RoundedRectangleBorder(
+                            side: const BorderSide(color: Colors.grey, width: 0.5),
+                            borderRadius: BorderRadius.circular(20.0),
+                        ),
+                        child: Padding(
+                            padding: EdgeInsets.all(verticalPadding),
+                            child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: itemContents,
+                            ),
+                        ),
+                    ),
                 ),
-                child: Padding(
-                  padding: const EdgeInsets.all(15.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: itemContents,
-                  ),
-                ),
-              ),
-            )));
-  }
+            ),
+        );
+    }
 }
