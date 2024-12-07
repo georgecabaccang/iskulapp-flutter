8e80e314ab806617ba268fab49792d292ca8ca37
Kelv1nG <kelvinmaglipon@gmail.com>
2024-12-07 17:52:44 +0800

    update sections

--- lib/models/tables/sections_table.dart
+++ lib/models/tables/sections_table.dart
@@ -2,6 +2,7 @@ import 'package:powersync/powersync.dart';
 
 const sectionsTable = Table('sections', [
   Column.text('academic_year_id'),
+  Column.text('grade_level_id'),
   Column.text('advisor_id'),
   Column.text('name'),
 ]);
