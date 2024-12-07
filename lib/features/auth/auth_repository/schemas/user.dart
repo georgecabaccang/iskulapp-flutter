877f6de7381fa88c4c0e75162ca0136d0d67c7cb
Kelv1nG <kelvinmaglipon@gmail.com>
2024-12-07 14:45:40 +0800

    add additional items in user schema

--- lib/features/auth/auth_repository/schemas/user.dart
+++ lib/features/auth/auth_repository/schemas/user.dart
@@ -6,15 +6,17 @@ part 'user.g.dart';
 @freezed
 class AuthenticatedUser with _$AuthenticatedUser {
   const factory AuthenticatedUser({
-    required int id,
+    required String id,
     required String email,
     required String role,
+    @JsonKey(name: 'login_type') required String loginType,
+    @JsonKey(name: 'login_id') required String loginId,
     @JsonKey(name: 'first_name') required String firstName,
     @JsonKey(name: 'last_name') required String lastName,
+    @JsonKey(name: 'school_id') required String schoolId,
     @JsonKey(name: 'school_name') required String schoolName,
-    @JsonKey(name: 'academic_year') required String academicYear,
-    @JsonKey(name: 'login_type') String? loginType,
-    @JsonKey(name: 'login_id') String? loginId,
+    @JsonKey(name: 'academic_year_id') required String academicYearId,
+    @JsonKey(name: 'academic_year_name') required String academicYearName,
   }) = _AuthenticatedUser;
 
   factory AuthenticatedUser.fromJson(Map<String, dynamic> json) =>
