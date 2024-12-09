ff4965d1c6e52c187fe859967048deb6f595860a
georgesycabaccang <georgesycabaccang@gmail.com>
2024-12-09 15:04:46 +0800

    Add padding for AppContent for uniform padding of widgets.

--- lib/pages/common_widgets/app_content.dart
+++ lib/pages/common_widgets/app_content.dart
@@ -1,38 +1,41 @@
 import 'package:flutter/material.dart';
 
 class AppContent extends StatelessWidget {
-  final List<Widget> content;
-  final bool isScrollable;
+    final List<Widget> content;
+    final bool isScrollable;
 
-  const AppContent({
-    super.key,
-    required this.content,
-    this.isScrollable = false,
-  });
+    const AppContent({
+        super.key,
+        required this.content,
+        this.isScrollable = false,
+    });
 
-  @override
-  Widget build(BuildContext context) {
-    return Expanded(
-      child: ClipRRect(
-        borderRadius: const BorderRadius.only(
-          topLeft: Radius.circular(20.0),
-          topRight: Radius.circular(20.0),
-        ),
-        child: Container(
-          decoration: const BoxDecoration(
-            color: Colors.white,
-          ),
-          child: isScrollable
-              ? SingleChildScrollView(
-                  child: Column(
-                    children: content,
-                  ),
-                )
-              : Column(
-                  children: content,
+    @override
+    Widget build(BuildContext context) {
+        return Expanded(
+            child: ClipRRect(
+                borderRadius: const BorderRadius.only(
+                    topLeft: Radius.circular(20.0),
+                    topRight: Radius.circular(20.0),
                 ),
-        ),
-      ),
-    );
-  }
+                child: Container(
+                    decoration: const BoxDecoration(
+                        color: Colors.white,
+                    ),
+                    child: Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 10),
+                        child: isScrollable
+                            ? SingleChildScrollView(
+                                child: Column(
+                                    children: content,
+                                ),
+                            )
+                            : Column(
+                                children: content,
+                            ),
+                    )
+                ),
+            ),
+        );
+    }
 }
