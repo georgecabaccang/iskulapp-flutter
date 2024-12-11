4e0e50fa195b1e991668aea6829fd330d6a3699c
georgesycabaccang <georgesycabaccang@gmail.com>
2024-12-11 14:17:04 +0800

    Adjust properties of questions in question.json and update assignment_question.dart for json conversion.

--- lib/pages/assignment/assignment_answers/assignment_answers_page.dart
+++ lib/pages/assignment/assignment_answers/assignment_answers_page.dart
@@ -1,142 +1,143 @@
 import 'dart:convert';
 import 'package:flutter/services.dart' show rootBundle;
 import 'package:flutter/material.dart';
+import 'package:school_erp/pages/assignment/helpers/classes/assignment_question.dart';
 import 'package:school_erp/theme/colors.dart';
 import 'package:school_erp/pages/assignment/assignment_check_page/widgets/student_item.dart';
 import 'widgets/assignment_card.dart';
 
 class AssignmentAnswersPage extends StatefulWidget {
-  final Student student;
+    // final Student student;
 
-  const AssignmentAnswersPage({super.key, required this.student});
+    const AssignmentAnswersPage({super.key, 
+        // required this.student
+    });
 
-  @override
-  createState() => _AssignmentAnswersPageState();
+    @override
+    createState() => _AssignmentAnswersPageState();
 }
 
 class _AssignmentAnswersPageState extends State<AssignmentAnswersPage> {
-  List<dynamic> questions = [];
-  int currentQuestionIndex = 0;
-  int? selectedOption;
+    List<AssignmentQuestion> questions = [];
+    int currentQuestionIndex = 0;
+    int? selectedOption;
 
-  @override
-  void initState() {
-    super.initState();
-    _loadQuestions();
-  }
+    @override
+    void initState() {
+        super.initState();
+        _loadQuestions();
+    }
 
-  Future<void> _loadQuestions() async {
-    String jsonString = await rootBundle.loadString('assets/questions.json');
-    final List<dynamic> jsonResponse = json.decode(jsonString);
-    setState(() {
-      questions = jsonResponse;
-      // Automatically select the correct option for the first question
-      if (questions.isNotEmpty && questions[0]['type'] == 'multi') {
-        final correctOption = questions[0]['answers']
-            .firstWhere((option) => option['is_correct'] == 1);
-        selectedOption = correctOption['id'];
-      }
-    });
-  }
+    Future<void> _loadQuestions() async {
+        String jsonString = await rootBundle.loadString('assets/questions.json');
+        List<dynamic> jsonResponse = json.decode(jsonString);
+        List<AssignmentQuestion> assignmentQuestions = jsonResponse.map((item) {
+                return AssignmentQuestion.fromJson(item);
+            }).toList();
 
-  void _handleBackPress(BuildContext context) {
-    Navigator.pop(context);
-  }
+        setState(() {
+                questions = assignmentQuestions;
+            });
+    }
 
-  void _nextQuestion() {
-    if (currentQuestionIndex < questions.length - 1) {
-      setState(() {
-        currentQuestionIndex++;
-        // Automatically select the correct answer for the next question
-        if (questions[currentQuestionIndex]['type'] == 'multi') {
-          final correctOption = questions[currentQuestionIndex]['answers']
-              .firstWhere((option) => option['is_correct'] == 1);
-          selectedOption = correctOption['id'];
+    void _handleBackPress(BuildContext context) {
+        Navigator.pop(context);
+    }
+
+    void _nextQuestion() {
+        if (currentQuestionIndex < questions.length - 1) {
+            setState(() {
+                    // currentQuestionIndex++;
+                    // // Automatically select the correct answer for the next question
+                    // if (questions[currentQuestionIndex]['type'] == 'multi') {
+                    //   final correctOption = questions[currentQuestionIndex]['answers']
+                    //       .firstWhere((option) => option['is_correct'] == 1);
+                    //   selectedOption = correctOption['id'];
+                    // } else {
+                    //   selectedOption = null;
+                    // }
+                });
         } else {
-          selectedOption = null;
+            _handleSubmit(context, questions[currentQuestionIndex]);
         }
-      });
-    } else {
-      _handleSubmit(context, questions[currentQuestionIndex]);
     }
-  }
 
-  void _handleSubmit(BuildContext context, dynamic question) {
-    // Handle submission logic
-  }
-
-  void _updateQuestion() {
-    // Handle update logic
-  }
-
-  void _onOptionSelected(int optionId) {
-    setState(() {
-      selectedOption = optionId;
-    });
-  }
-
-  QuestionType _getQuestionType(String type) {
-    switch (type) {
-      case 'multi':
-        return QuestionType.multipleChoice;
-      case 'essay':
-        return QuestionType.essay;
-      case 'short':
-        return QuestionType.shortAnswer;
-      case 'true_false':
-        return QuestionType.trueOrFalse;
-      default:
-        return QuestionType.multipleChoice;
+    void _handleSubmit(BuildContext context, dynamic question) {
+        // Handle submission logic
     }
-  }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      // appBar: CustomAppBar(
-      //   title: "${widget.student.name} Assignment Answers",
-      //   onBackPressed: () => _handleBackPress(context),
-      //   titleStyle: const TextStyle(color: Colors.white, fontSize: 18.0),
-      //   trailingWidget: Text(
-      //     'Section 1',
-      //     style: bodyStyle().copyWith(color: Colors.white, fontSize: 16),
-      //   ),
-      // ),
-      body: Stack(
-        children: [
-          Container(color: AppColors.primaryColor),
-          Container(
-            decoration: const BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(30.0),
-                topRight: Radius.circular(30.0),
-              ),
-            ),
-            child: questions.isEmpty
-                ? const Center(child: CircularProgressIndicator())
-                : Column(
-                    children: [
-                      Expanded(
-                        // Make sure the AssignmentCard takes the full space
-                        child: AssignmentCard(
-                          question: questions[currentQuestionIndex],
-                          currentQuestionIndex: currentQuestionIndex,
-                          totalQuestions: questions.length,
-                          selectedOption: selectedOption,
-                          onOptionSelected: _onOptionSelected,
-                          onNextPressed: _nextQuestion,
-                          onUpdatePressed: _updateQuestion,
-                          questionType: _getQuestionType(
-                              questions[currentQuestionIndex]['type']),
-                          isInteractionEnabled: false,
+    void _updateQuestion() {
+        // Handle update logic
+    }
+
+    void _onOptionSelected(int optionId) {
+        setState(() {
+                selectedOption = optionId;
+            });
+    }
+
+    // QuestionType _getQuestionType(String type) {
+    //   switch (type) {
+    //     case 'multi':
+    //       return QuestionType.multipleChoice;
+    //     case 'essay':
+    //       return QuestionType.essay;
+    //     case 'short':
+    //       return QuestionType.shortAnswer;
+    //     case 'true_false':
+    //       return QuestionType.trueOrFalse;
+    //     default:
+    //       return QuestionType.multipleChoice;
+    //   }
+    // }
+
+    @override
+    Widget build(BuildContext context) {
+        return Scaffold(
+            // appBar: CustomAppBar(
+            //   title: "${widget.student.name} Assignment Answers",
+            //   onBackPressed: () => _handleBackPress(context),
+            //   titleStyle: const TextStyle(color: Colors.white, fontSize: 18.0),
+            //   trailingWidget: Text(
+            //     'Section 1',
+            //     style: bodyStyle().copyWith(color: Colors.white, fontSize: 16),
+            //   ),
+            // ),
+            body: Stack(
+                children: [
+                    Container(color: AppColors.primaryColor),
+                    Container(
+                        decoration: const BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.only(
+                                topLeft: Radius.circular(30.0),
+                                topRight: Radius.circular(30.0),
+                            ),
                         ),
-                      ),
-                    ],
-                  ),
-          ),
-        ],
-      ),
-    );
-  }
+                        child: questions.isEmpty
+                            ? const Center(child: CircularProgressIndicator())
+                            : Column(
+                            // children: [
+                            //   Expanded(
+                            //     // Make sure the AssignmentCard takes the full space
+                            //     child: AssignmentCard(
+                            //       question: questions[currentQuestionIndex],
+                            //       currentQuestionIndex: currentQuestionIndex,
+                            //       totalQuestions: questions.length,
+                            //       selectedOption: selectedOption,
+                            //       onOptionSelected: _onOptionSelected,
+                            //       onNextPressed: _nextQuestion,
+                            //       onUpdatePressed: _updateQuestion,
+                            //       questionType: _getQuestionType(
+                            //           questions[currentQuestionIndex]['type']),
+                            //       isInteractionEnabled: false,
+                            //     ),
+                            //   ),
+                            // ],
+                            ),
+                    ),
+                ],
+            ),
+        );
+    }
 }
--- lib/pages/assignment/helpers/classes/assignment_question.dart
+++ lib/pages/assignment/helpers/classes/assignment_question.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: non_constant_identifier_names
+
 enum QuestionType  {
     multipleChoice,
     essay,
@@ -6,17 +8,60 @@ enum QuestionType  {
 }
 
 class AssignmentQuestion {
-    final QuestionType questionType;
-    final String question;
-    final List<String>? options;
-    final String answer;
-    final int points;
+    final QuestionType type;  
+    final String question;    
+    final List<Answers> answers;    
+    final int points;         
 
-    AssignmentQuestion(
-        this.questionType, 
-        this.question, 
-        this.options,
-        this.answer, 
-        this.points, 
-    );
+    AssignmentQuestion(this.type, this.question, this.answers, this.points);
+
+    factory AssignmentQuestion.fromJson(Map<String, dynamic> json) {
+        QuestionType questionType = _mapStringToQuestionType(json['type']);
+
+        return AssignmentQuestion(
+            questionType,
+            json['question'] ?? '',  
+            (json['answers'] as List).map((answerJson) => Answers.fromJson(answerJson)).toList(),
+            json['points'] ?? 0,  
+        );
+    }
+
+    static QuestionType _mapStringToQuestionType(String type) {
+        switch (type) {
+            case 'multipleChoice':
+                return QuestionType.multipleChoice;
+            case 'essay':
+                return QuestionType.essay;
+            case 'shortAnswer':
+                return QuestionType.shortAnswer;
+            case 'trueOrFalse':
+                return QuestionType.trueOrFalse;
+            default:
+            return QuestionType.multipleChoice;
+        }
+    }
+}
+
+class Answers  {
+
+    final String text;
+    final int is_correct;
+    final int is_student_answered;
+
+    Answers(
+        this.text, 
+        this.is_correct, 
+        this.is_student_answered
+    ); 
+
+    factory Answers.fromJson(Map<String, dynamic> json) {
+        return Answers(
+            json['text'] ?? '',  
+            json['is_correct'] ?? 0,  
+            json['is_student_answered'] ?? 0, 
+        );
+    }
+
+    int get isCorrect => is_correct;
+    int get isStudentAnswered => is_student_answered;
 }
