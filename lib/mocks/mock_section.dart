2403aca437d0debb396020889958c150a39634f5
georgesycabaccang <georgesycabaccang@gmail.com>
2024-12-16 18:41:48 +0800

    Move location of mock stuff for attendance and create MockSections in mock_section.dart

--- lib/mocks/mock_section.dart
+++ lib/mocks/mock_section.dart
@@ -1,3 +1,17 @@
+class MockSections {
+    final List<MockSection> mockSections;
+
+    MockSections({required this.mockSections});
+
+    factory MockSections.fromJson(List<dynamic> json) {
+        List<MockSection> sections = json.map((sectionJson) {
+                return MockSection.fromJson(sectionJson);
+            }).toList();
+
+        return MockSections(mockSections: sections);
+    }
+}
+
 class MockSection {
     final int id;
     final String name;
