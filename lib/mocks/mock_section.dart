47f07683c3173510bbf7f4311d1cff3a746214cd
georgesycabaccang <georgesycabaccang@gmail.com>
2024-12-17 10:59:12 +0800

    Remove students field from MockSection.

--- lib/mocks/mock_section.dart
+++ lib/mocks/mock_section.dart
@@ -15,15 +15,13 @@ class MockSections {
 class MockSection {
     final int id;
     final String name;
-    final List<int> studentIds;
 
-    MockSection({required this.id, required this.name, required this.studentIds});
+    MockSection({required this.id, required this.name});
 
     factory MockSection.fromJson(Map<String, dynamic> json) {
         return MockSection(
             id: json['id'], 
             name: json['name'], 
-            studentIds: json['students']
         );
     }
 }
