6aa2f2e3c13a852f627b11c54b68e1a725043bbc
georgesycabaccang <georgesycabaccang@gmail.com>
2024-12-20 00:50:40 +0800

    Rename MockRoles to MockRole and create MockRoles.

--- lib/mocks/mock_roles.dart
+++ lib/mocks/mock_roles.dart
@@ -1,13 +1,27 @@
 import 'package:school_erp/interfaces/display_values.dart';
 
-class MockRoles implements DisplayValues{
+class MockRoles {
+    final List<MockRole> mockRoles;
+
+    MockRoles({required this.mockRoles});
+
+    factory MockRoles.fromJson(List<dynamic> json) {
+        List<MockRole> roles = json.map((roleJson) {
+                return MockRole.fromJson(roleJson);
+            }).toList();
+
+        return MockRoles(mockRoles: roles);
+    }
+}
+
+class MockRole implements DisplayValues{
     final String id;
     final String role;
 
-    MockRoles({required this.id, required this.role});
+    MockRole({required this.id, required this.role});
 
-    factory MockRoles.fromJson(Map<String, dynamic> json) {
-        return MockRoles(
+    factory MockRole.fromJson(Map<String, dynamic> json) {
+        return MockRole(
             id: json['id'] ?? '', 
             role: json['role'] ?? ''
         );
