498473657692d0bab975e527f941fdf7667de798
georgesycabaccang <georgesycabaccang@gmail.com>
2024-12-26 15:47:18 +0800

    Create reusable CustomRangePicker for date filtering.

---
+++ lib/pages/common_widgets/forms/calendar_range_picker/custom_range_picker.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:school_erp/theme/colors.dart';
+
+class CustomRangePicker {
+    static Future<DateTimeRange?> showPicker(BuildContext context) async {
+        final DateTimeRange? result = await showDateRangePicker(
+            context: context,
+            firstDate: DateTime(2000, 1, 1),
+            lastDate: DateTime(2100, 12, 31),
+            currentDate: DateTime.now(),
+            saveText: 'Done',
+            builder: (context, child) {
+                return Theme(
+                    data: ThemeData(
+                        colorScheme: ColorScheme.light(
+                            primary: AppColors.primaryColor,
+                            surface: AppColors.whiteColor,
+                            onSurface: AppColors.primaryColor,
+                        ),
+                        textTheme: TextTheme(
+                            bodyMedium: TextStyle(color: Colors.black),
+                        ),
+                        datePickerTheme: DatePickerThemeData(
+                            rangeSelectionBackgroundColor: AppColors.primaryColor.withOpacity(0.2),
+                            rangePickerBackgroundColor: AppColors.whiteColor,
+                            rangePickerHeaderBackgroundColor: AppColors.primaryColor,
+                            rangePickerHeaderForegroundColor: AppColors.whiteColor,
+                        ),
+                    ),
+                    child: child!,
+                );
+            },
+        );
+
+        return result;
+    }
+}
