013be0d2ec4e2c5ba3aa9e82c8b780fbeb848c62
Kelv1nG <kelvinmaglipon@gmail.com>
2025-01-05 11:56:09 +0800

    add font color to coming soon widget

--- lib/pages/common_widgets/views/coming_soon_widget.dart
+++ lib/pages/common_widgets/views/coming_soon_widget.dart
@@ -2,19 +2,28 @@ import 'package:flutter/material.dart';
 import 'package:school_erp/theme/colors.dart';
 
 class ComingSoonWidget extends StatelessWidget {
+  final String title;
+  final Color? fontColor;
+
+  const ComingSoonWidget({
+    required this.title,
+    this.fontColor = AppColors.whiteColor,
+    super.key,
+  });
+
   @override
   Widget build(BuildContext context) {
     return Center(
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          const Text(
-            'Chat rooms are under construction!!!',
+          Text(
+            '$title is under construction!!!',
             textAlign: TextAlign.center,
             style: TextStyle(
               fontSize: 24,
               fontWeight: FontWeight.bold,
-              color: AppColors.whiteColor,
+              color: fontColor,
             ),
           ),
           const SizedBox(height: 16),
@@ -22,13 +31,13 @@ class ComingSoonWidget extends StatelessWidget {
             'assets/images/under_construction.webp',
           ),
           const SizedBox(height: 16),
-          const Text(
+          Text(
             "Thank you for your patience! \n Stay tuned for what's coming soon!",
             textAlign: TextAlign.center,
             style: TextStyle(
               fontSize: 18,
               fontWeight: FontWeight.bold,
-              color: AppColors.whiteColor,
+              color: fontColor,
             ),
           ),
         ],
