9751a8b66ceba6e13377fd04ad202d55324a4f1d
georgesycabaccang <georgesycabaccang@gmail.com>
2025-01-06 14:39:48 +0800

    Create display for attendance details in attendance_list_item_card.dart.

--- lib/pages/attendance/attendance_calendar/widgets/attendance_list_item_card.dart
+++ lib/pages/attendance/attendance_calendar/widgets/attendance_list_item_card.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:school_erp/pages/attendance/attendance_calendar/helpers/classes/attendance_list_item_data.dart';
 import 'package:school_erp/pages/common_widgets/cards/custom_item_card.dart';
+import 'package:school_erp/theme/colors.dart';
 
 class AttendanceListItemCard extends StatelessWidget{
     final AttendanceListItemData attendanceData;
@@ -10,12 +11,52 @@ class AttendanceListItemCard extends StatelessWidget{
         required this.attendanceData
     });
 
+    Widget _buildCircle(int label, Color color) {
+        return Container(
+            width: 25,
+            height: 25,
+            decoration: BoxDecoration(
+                color: color,
+                shape: BoxShape.circle,
+            ),
+            child: Center(
+                child: Text(
+                    label.toString(),
+                    style: TextStyle(
+                        color: AppColors.whiteColor,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 12,
+                    ),
+                ),
+            ),
+        );
+    }
+
     @override
     Widget build(BuildContext context) {
         return CustomItemCard(
             itemContents: [
-                Text(attendanceData.student.displayName)
-            ]);
+                Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                        Column(
+                            children: [
+                                Text(attendanceData.student.displayName)
+                            ],
+                        ),
+                        Column(children: [
+                                Row(
+                                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                                    children: [
+                                        _buildCircle(attendanceData.totalPresent, Colors.green.withOpacity(0.5)),
+                                        _buildCircle(attendanceData.totalLate, Color.fromARGB(255, 240, 190, 50).withOpacity(0.7) ), 
+                                        _buildCircle(attendanceData.totalAbsent, Color.fromARGB(220, 0, 190, 210)),
+                                        _buildCircle(attendanceData.totalLeave, Colors.red.withOpacity(0.7))],)
+                            ],
+                        )
+                    ],
+                )
+            ]
+        );
     }
-
 }
