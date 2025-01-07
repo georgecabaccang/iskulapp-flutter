6a4238edc6b46ab17392373901eaef570a6757cb
georgesycabaccang <georgesycabaccang@gmail.com>
2025-01-07 16:36:07 +0800

    Update how students' names are displayed, and added new student samples to test changes.

--- lib/mocks/mock_student.dart
+++ lib/mocks/mock_student.dart
@@ -3,15 +3,26 @@ import 'package:school_erp/interfaces/display_values.dart';
 class MockStudent implements EntityDisplayData {
     final String id;
     final String firstName;
+    final String? secondName;
+    final String? middleName;
     final String lastName;
     final String sectionId;
 
-    MockStudent({required this.id, required this.firstName, required this.lastName, required this.sectionId});
+    MockStudent({
+        required this.id, 
+        required this.firstName,
+        this.secondName,
+        this.middleName, 
+        required this.lastName, 
+        required this.sectionId
+    });
 
     factory MockStudent.fromJson(Map<String, dynamic> json) {
         return MockStudent(
             id: json['id'], 
             firstName: json['first_name'], 
+            secondName: json['second_name'], 
+            middleName: json['middle_name'], 
             lastName: json['last_name'],
             sectionId: json['section_id'], 
         );
@@ -21,7 +32,7 @@ class MockStudent implements EntityDisplayData {
     String get value => id;
 
     @override
-    String get displayName => "$firstName $lastName";
+    String get displayName => "${lastName.toUpperCase()}, $firstName ${secondName ?? ''} ${middleName != null ? '${middleName![0].toUpperCase() }.' : ''}";
 
 
 }
